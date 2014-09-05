1716d3055d0890b6772237113751295b9f63ea62
hbaughman <hbaughman741@gmail.com>
2014-09-05 08:13:28 -0400

    Add arriving/departing_flights spec coverage

--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -14,4 +14,19 @@ describe Airport do
     it { should_not be_valid }
   end
 
+  describe "should list departing/arriving flights" do
+    before do
+      @airport.save
+      @destination = Airport.create(airport_code: "LAX")
+      @flight = Flight.create flight_number: 616, duration: 4.5, 
+                          departure_time: Time.new,
+                          from_airport: @airport, to_airport: @destination
+    end
+
+    specify { expect(@airport.departing_flights).to include(@flight) }
+    specify { expect(@destination.arriving_flights).to include(@flight) }
+    
+
+  end
+
 end
